a88a14ff812a5a85fbd4c0606d84c7f4e2d59907
psy_wombats <psy_wombats@wombatrpgs.net>
2012-08-06 01:35:20 -0700

    Project setup

---
+++ RainfallSchema/src/net/wombatrpgs/rainfallschema/Test1MDO.java
@@ -0,0 +1,22 @@
+/**
+ *  Test1MDO.java
+ *  Created on Aug 4, 2012 9:38:51 PM for project RainfallSchema
+ *  Author: psy_wombats
+ *  Contact: psy_wombats@wombatrpgs.net
+ */
+package net.wombatrpgs.rainfallschema;
+
+import net.wombatrpgs.mgns.core.Annotations.*;
+
+/**
+ * Just testin'
+ */
+public class Test1MDO {
+	
+	@DisplayName("Herp.")
+	@Desc("descript text goes here.")
+	@DefaultValue("hahahaha default")
+	@Nullable(true)
+	public String herpaderp;
+
+}
