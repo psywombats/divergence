dc450ccb90c702adf0078b6cda19d5492786eb1c
psy_wombats <psy_wombats@wombatrpgs.net>
2012-08-07 02:33:56 -0700

    more test schema

---
+++ RainfallSchema/src/net/wombatrpgs/rainfallschema/MonsterMDO.java
@@ -0,0 +1,34 @@
+/**
+ *  MonsterMDO.java
+ *  Created on Aug 7, 2012 2:21:51 AM for project RainfallSchema
+ *  Author: psy_wombats
+ *  Contact: psy_wombats@wombatrpgs.net
+ */
+package net.wombatrpgs.rainfallschema;
+
+import net.wombatrpgs.mgns.core.Annotations.*;
+
+/**
+ * Another test schema...
+ */
+public class MonsterMDO {
+	
+	@Desc("Visible name of the monster")
+	public String monsterName;
+	
+	@Desc("Text displayed in bestiary, or empty if no entry")
+	@EmptyAllowed
+	public String bestiaryEntry;
+	
+	@Desc("Maximum health of monster")
+	public int mhp;
+	
+	@Desc("Gold dropped when killed")
+	public int gold;
+	
+	@Desc("Monster that this monster can summon via special attack, or none if no special")
+	@Nullable
+	public MonsterMDO summonedMonster;
+	
+
+}
