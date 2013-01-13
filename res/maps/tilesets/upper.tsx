24609e724c8504d36a880ec0071fa9c19004d9b5
Zak <zking@bu.edu>
2013-01-12 22:28:30 -0500

    Tilesets updated

---
+++ res/maps/tilesets/upper.tsx
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="upper" tilewidth="32" tileheight="32">
+ <image source="upper.PNG" trans="8080ff" width="256" height="192"/>
+ <tile id="8">
+  <properties>
+   <property name="x" value="1337"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+</tileset>
---
+++ res/maps/tilesets/wood.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wood2" tilewidth="32" tileheight="32">
+ <image source="wood2.PNG" trans="8080ff" width="320" height="192"/>
+</tileset>
