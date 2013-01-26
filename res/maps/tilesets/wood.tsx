19d109948c52e2b031d70e25600376941493d513
psy_wombats <psy_wombats@wombatrpgs.net>
2013-01-26 04:49:59 -0500

    Sort-of added support for 48 height cliffs

--- res/maps/tilesets/wood.tsx
+++ res/maps/tilesets/wood.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="wood2" tilewidth="32" tileheight="32">
- <image source="wood2.PNG" trans="8080ff" width="320" height="192"/>
+<tileset name="wood" tilewidth="32" tileheight="32">
+ <image source="wood2.PNG" trans="8080ff" width="320" height="224"/>
+ <tile id="60">
+  <properties>
+   <property name="top" value=""/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="top" value=""/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="top" value=""/>
+  </properties>
+ </tile>
 </tileset>
