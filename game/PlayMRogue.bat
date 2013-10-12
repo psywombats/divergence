627b49f4c19923ca3581022c32aaeb3d07007cb4
aking <psy_wombats@wombatrpgs.net>
2013-10-12 04:46:35 -0400

    Fixing up some jar issues. Oh and MONSTER NAMES

--- game/PlayMRogue.bat
+++ game/PlayMRogue.bat
@@ -1,2 +1,11 @@
-java -jar MRogue.jar
+@echo off
+IF EXIST "C:\Program Files\Java\jre6\bin\java.exe" (
+"C:\Program Files\Java\jre6\bin\java.exe" -jar MRogue.jar
+) ELSE IF EXIST "C:\Program Files\Java\jre7\bin\java.exe" (
+"C:\Program Files\Java\jre7\bin\java.exe" -jar MRogue.jar
+) ELSE (
+@echo on
+echo You don't appear to have Java installed, do that first!
+)
+@echo on
 PAUSE
