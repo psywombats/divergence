4217f378b4cd3ddcb5f8867e6b193ed9648c21b8
aking <psy_wombats@wombatrpgs.net>
2014-02-10 21:03:23 -0500

    Splitting things into a "tactics" and "saga" project!

---
+++ src/tactics-desktop/src/net/wombatrpgs/tactics/core/SagaGame.java
@@ -0,0 +1,16 @@
+/**
+ *  SagaGame.java
+ *  Created on Feb 10, 2014 12:29:13 AM for project saga-desktop
+ *  Author: psy_wombats
+ *  Contact: psy_wombats@wombatrpgs.net
+ */
+package net.wombatrpgs.tactics.core;
+
+import net.wombatrpgs.mgne.core.MgnGame;
+
+/**
+ * One day, this class will tell MGNE how to run Saga.
+ */
+public class SagaGame extends MgnGame {
+
+}
