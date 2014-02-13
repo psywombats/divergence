da55ba6df31a8bbb3288b6d9bdb1facd90201dac
aking <psy_wombats@wombatrpgs.net>
2014-02-12 19:26:25 -0500

    More energy corrections... of course

--- src/tactics-desktop/src/net/wombatrpgs/tactics/rpg/Battle.java
+++ src/tactics-desktop/src/net/wombatrpgs/tactics/rpg/Battle.java
@@ -9,6 +9,7 @@ package net.wombatrpgs.tactics.rpg;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.wombatrpgs.mgne.core.interfaces.Updateable;
 import net.wombatrpgs.mgne.io.CommandListener;
 import net.wombatrpgs.mgne.maps.Level;
 import net.wombatrpgs.mgneschema.io.data.InputCommand;
@@ -18,7 +19,8 @@ import net.wombatrpgs.tactics.maps.TacticsMap;
  * Contains information on the ongoing battle. There's no need to make the map
  * do the heavy lifting as far as turn order etc is concerned.
  */
-public class Battle implements CommandListener {
+public class Battle implements	CommandListener,
+								Updateable {
 	
 	protected TacticsMap map;
 	
@@ -33,6 +35,33 @@ public class Battle implements CommandListener {
 	public Battle(Level level) {
 		map = new TacticsMap(level);
 		allCombatants = new ArrayList<GameUnit>();
+	}
+
+	/**
+	 * @see net.wombatrpgs.mgne.core.interfaces.Updateable#update(float)
+	 * At the moment, we're just going to handle turn progression and leave
+	 * things like "are we initialized yet" somewhere else.
+	 */
+	@Override
+	public void update(float elapsed) {
+		if (actor == null) {
+			actor = nextActor();
+		}
+		int energySpent = actor.doneWithTurn();
+		if (energySpent >= 0) {
+			// the actor actually did something and is done
+			// grant everyone else energy equal to energy expended
+			for (GameUnit unit : allCombatants) {
+				if (unit != actor) {
+					unit.grantEnergy(energySpent);
+				}
+			}
+			// keep a 0-centered energy standard (ie, last mover is at 0)
+			int energyCorrection = actor.getEnergy();
+			for (GameUnit unit : allCombatants) {
+				unit.grantEnergy(-energyCorrection);
+			}
+		}
 	}
 
 	/**
