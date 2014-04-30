b05ab5504223592e499bb35e8d0794a36aa952ce
crumley_ <zking@bu.edu>
2014-04-30 01:27:47 -0400

    Exported tilesets and updated test maps to new art

---
+++ saga/res/maps/tilesets/lower.tsx
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lower" tilewidth="16" tileheight="16">
+ <image source="lower2.PNG" width="112" height="128"/>
+ <tile id="3">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="x" value=""/>
+  </properties>
+ </tile>
+</tileset>
