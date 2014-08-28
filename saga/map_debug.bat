642ded4da21122f10cc8210df25b20152854ae9f
aking <psy_wombats@wombatrpgs.net>
2014-08-27 22:45:06 -0700

    Adding your party members at the gates to town

--- saga/map_debug.bat
+++ saga/map_debug.bat
@@ -5,13 +5,13 @@ Set /P y= start y:
 
 @echo off
 IF EXIST "C:\Program Files\Java\jre7\bin\java.exe" (
-"C:\Program Files\Java\jre7\bin\java.exe" -jar game.jar map=%map% x=%x% y=%y%
+"C:\Program Files\Java\jre7\bin\java.exe" -jar game.jar party=party_playerTest debug=develop map=%map% x=%x% y=%y%
 ) ELSE IF EXIST "C:\Program Files\Java\jre6\bin\java.exe" (
-"C:\Program Files\Java\jre6\bin\java.exe" -jar game.jar map=%map% x=%x% y=%y%
+"C:\Program Files\Java\jre6\bin\java.exe" -jar game.jar party=party_playerTest debug=develop map=%map% x=%x% y=%y%
 ) ELSE IF EXIST "C:\Program Files (x86)\Java\jre6\bin\java.exe" (
-"C:\Program Files (x86)\Java\jre6\bin\java.exe" -jar game.jar map=%map% x=%x% y=%y%
+"C:\Program Files (x86)\Java\jre6\bin\java.exe" -jar game.jar party=party_playerTest debug=develop map=%map% x=%x% y=%y%
 ) ELSE IF EXIST "C:\Program Files (x86)\Java\jre7\bin\java.exe" (
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar game.jar map=%map% x=%x% y=%y%
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar game.jar party=party_playerTest debug=develop map=%map% x=%x% y=%y%
 ) ELSE (
 PAUSE
 @echo on
