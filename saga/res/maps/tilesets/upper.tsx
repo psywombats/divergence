91ba18149ac52a40dbaf78cd93da3a3e92696b40
aking <psy_wombats@wombatrpgs.net>
2014-09-06 21:04:45 -0700

    Stop squashing the tileset transparency!

--- saga/res/maps/tilesets/upper.tsx
+++ saga/res/maps/tilesets/upper.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="upper" tilewidth="16" tileheight="16">
- <image source="upper2.PNG" trans="ff0080" width="144" height="144"/>
+ <image source="upper2.PNG" width="144" height="144"/>
  <tile id="1">
   <properties>
    <property name="x" value=""/>
