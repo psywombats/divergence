360bc09790ebbbbc291dc0fcf48db723efff2208
aking <psy_wombats@wombatrpgs.net>
2014-10-04 14:44:56 -0700

    Getting GME-java compiling

---
+++ src/gme-java/src/net/wombatrpgs/gme/GmeTests.java
@@ -0,0 +1,22 @@
+/**
+ *  GmeTests.java
+ *  Created on Oct 4, 2014 2:44:32 PM for project gme-java
+ *  Author: psy_wombats
+ *  Contact: psy_wombats@wombatrpgs.net
+ */
+package net.wombatrpgs.gme;
+
+/**
+ *
+ */
+public class GmeTests {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
