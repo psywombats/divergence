6554b747259c395516ce6f6c9557f9cf3dbb2a1c
aking <psy_wombats@wombatrpgs.net>
2015-01-16 18:50:06 -0800

    Version number update

--- src/saga-desktop/src/net/wombatrpgs/saga/core/SConstants.java
+++ src/saga-desktop/src/net/wombatrpgs/saga/core/SConstants.java
@@ -15,7 +15,7 @@ import net.wombatrpgs.mgne.core.MGlobal;
 public class SConstants extends Constants {
 	
 	/** Version info */
-	public static final String VERSION = "0.2";
+	public static final String VERSION = "0.201";
 	public static final int SAVE_BUILD = 7;
 	public static final String GAME_NAME = "saga";
 	
